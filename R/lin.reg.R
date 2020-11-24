6f55e814e14a7f9187d09789000983a6c0f2fcee
Samuel Higbee <35346245+samuelhigbee@users.noreply.github.com>
2020-11-23 20:03:44 -0500

    lin.reg.edits

--- R/lin.reg.R
+++ R/lin.reg.R
@@ -6,7 +6,12 @@
 #' @param X The explanatory variables
 #' @export
 
-lin.reg <- function(Y, X) {
-  beta <- solve( t(X) %*% X) %*% t(X) %*% Y
+lin.reg <- function(Y, X, const=TRUE) {
+  n.X <- as.matrix(X)
+  n.Y <- as.matrix(Y)
+  if (const==TRUE) {
+    n.X <- cbind(1,n.X)
+  }
+  beta <- solve( t(n.X) %*% n.X) %*% t(n.X) %*% n.Y
   return(beta)
 }
